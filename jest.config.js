e4f931759dae9742310152f5e4de495caa1723db
Satyendra Singh Naruka <sn.satyendra@gmail.com>
2020-05-05 20:48:15 +0530

    - Add coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   testEnvironment: 'node',
-  snapshotResolver: './test/helpers/snapshotResolver.js',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/src/*.js',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: './coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+  coverageReporters: ['cobertura', 'html', 'lcov'],
+  moduleFileExtensions: ['js'],
 };
