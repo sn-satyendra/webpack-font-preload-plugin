f7982c3130beb8bb8485cb46f5a6ce7653d3bfe0
Satyendra Singh Naruka <sn.satyendra@gmail.com>
2021-12-14 20:54:57 +0530

    - Adding test names - Adding 1 more type of font for tests

--- test/index.test.js
+++ test/index.test.js
@@ -15,4 +15,15 @@ describe("WebpackFontPreloadPlugin tests", () => {
       console.log(error);
     }
   });
+
+  /**
+   * 1. should preload all the available fonts when no configuration is specified
+   * 2. should preload only specific fonts when `extensions` is specified
+   * 3. should not add `crossorigin` attribute when option is turned off
+   * 4. should add link with `prefetch` when `loadType` is set as `prefetch`
+   * 5. should allow to add `links` at arbitrary location in dom
+   * 6. should allow to update the generated index.html by using a template placeholder
+   * 7. should allow to filter font's for preload by specifying a contained string
+   * 8. should allow to filter font's for preload by specifying a regex
+   */
 });
