fcc1b8078897d65418240efd234641bb45eacda0
Satyendra Singh Naruka <sn.satyendra@gmail.com>
2022-06-13 16:38:33 +0530

    - Formatting

--- jest.config.ts
+++ jest.config.ts
@@ -10,10 +10,10 @@ const config: Config.InitialOptions = {
   coverageDirectory: "./coverage",
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 95,
+      functions: 100,
+      lines: 95,
+      statements: 96,
     },
   },
   coverageReporters: ["cobertura", "html", "lcov"],
